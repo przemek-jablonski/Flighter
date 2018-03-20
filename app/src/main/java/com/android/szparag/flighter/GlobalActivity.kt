a58e8aa030b95d5dcfbe8350830037bf5e46cf33
ciemek <sharaquss@gmail.com>
2018-03-20 23:48:11 +0100

    code import

---
+++ app/src/main/java/com/android/szparag/flighter/GlobalActivity.kt
@@ -0,0 +1,12 @@
+package com.android.szparag.flighter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GlobalActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_global)
+  }
+}
