fd93e6a8a5b0da741122125d8dca8a68c10040b8
ciemek <sharaquss@gmail.com>
2018-03-21 01:10:37 +0100

    google map styled

--- flighter/src/main/java/com/android/szparag/flighter/GlobalActivity.kt
+++ flighter/src/main/java/com/android/szparag/flighter/GlobalActivity.kt
@@ -3,6 +3,8 @@ package com.android.szparag.flighter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.MapStyleOptions
 import kotlinx.android.synthetic.main.activity_global.*
 
 class GlobalActivity : AppCompatActivity() {
@@ -12,6 +14,7 @@ class GlobalActivity : AppCompatActivity() {
     setContentView(R.layout.activity_global)
     mapView.onCreate(savedInstanceState)
     mapView.getMapAsync { googleMap ->
+      googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.googlemapstyle))
       googleMap.uiSettings.apply {
         setAllGesturesEnabled(false)
       }
